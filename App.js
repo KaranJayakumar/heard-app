3587f29fb85b4d80f581d117bf685e2d66345920
KaranJayakumar <117019183+KaranJayakumar@users.noreply.github.com>
2024-01-06 16:20:51 +1100

    Implemented launch screen

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{headerShown : false}}>
                 <Stack.Screen name="Launch" component={Launch} />
             </Stack.Navigator>
         </NavigationContainer>
--- src/screens/launch.js
+++ src/screens/launch.js
@@ -1,11 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, ImageBackground, Text,Image, View} from 'react-native';
 import React from 'react';
 const Launch = () => {
     return(
-        <View>
-            <Text>Launch</Text>
-        </View>
+        <ImageBackground style = {styles.background}>
+            <Image source={require('../../assets/headphoneImg.png')} style = {styles.logo}/> 
+        </ImageBackground>
     )
 }
 export default Launch;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background : {
+        flex : 1,
+        alignItems : "center",
+        justifyContent : "center",
+        backgroundColor : "black",
+    },
+    logo :{
+        justifyContent : "center",
+    }
+});
